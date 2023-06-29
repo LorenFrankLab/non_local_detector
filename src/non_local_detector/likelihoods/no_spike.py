f69dd7aa2ea32804b044ac97efc8a9f4cf6a7830
Eric Denovellis <edeno@bu.edu>
2023-06-28 22:12:25 -0700

    Update for spike times

--- src/non_local_detector/likelihoods/no_spike.py
+++ src/non_local_detector/likelihoods/no_spike.py
@@ -3,12 +3,25 @@ import scipy.stats
 
 
 def predict_no_spike_log_likelihood(
-    spikes,
+    time,
+    spike_times: np.ndarray,
     no_spike_rate: float = 1e-10,
     sampling_frequency: float = 500.0,
 ):
-    n_neurons = spikes.shape[1]
+    n_neurons = len(spike_times)
     no_spike_rates = np.ones((n_neurons,)) * no_spike_rate / sampling_frequency
+    spike_count_per_time_bin = np.stack(
+        [
+            np.bincount(
+                np.digitize(neuron_spike_times, time[1:-1]),
+                minlength=time.shape[0],
+            )
+            for neuron_spike_times in spike_times
+        ],
+        axis=1,
+    )
     return np.sum(
-        scipy.stats.poisson.logpmf(spikes, no_spike_rates), axis=-1, keepdims=True
+        scipy.stats.poisson.logpmf(spike_count_per_time_bin, no_spike_rates),
+        axis=-1,
+        keepdims=True,
     )
