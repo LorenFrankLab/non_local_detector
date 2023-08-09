b007bf8a71813a5ebf6a15f684bfefc490e868cf
Eric Denovellis <edeno@bu.edu>
2023-08-09 09:08:21 -0700

    Add interactive visualization code

---
+++ src/non_local_detector/visualization/__init__.py
@@ -0,0 +1,7 @@
+from non_local_detector.visualization.figurl_1D import (
+    create_interactive_1D_decoding_figurl,
+)
+from non_local_detector.visualization.figurl_2D import (
+    create_interactive_2D_decoding_figurl,
+)
+from non_local_detector.visualization.static import plot_non_local_model
