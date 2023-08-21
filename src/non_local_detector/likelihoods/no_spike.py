9c95d8a52406ad185507999778f155ed9b3667d2
Eric Denovellis <edeno@bu.edu>
2023-08-20 17:39:07 -0700

    Add counter on no spike

--- src/non_local_detector/likelihoods/no_spike.py
+++ src/non_local_detector/likelihoods/no_spike.py
@@ -1,6 +1,7 @@
 import numpy as np
 import jax.numpy as jnp
 import jax.scipy
+from tqdm.autonotebook import tqdm
 
 
 def get_spikecount_per_time_bin(spike_times, time):
@@ -21,7 +22,9 @@ def predict_no_spike_log_likelihood(
     no_spike_rates = no_spike_rate * np.median(np.diff(time))
     no_spike_log_likelihood = jnp.zeros((time.shape[0],))
 
-    for neuron_spike_times in spike_times:
+    for neuron_spike_times in tqdm(
+        spike_times, unit="cell", desc="No Spike Likelihood"
+    ):
         no_spike_log_likelihood += (
             jax.scipy.special.xlogy(
                 get_spikecount_per_time_bin(neuron_spike_times, time), no_spike_rates
