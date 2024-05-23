76c5f257d9e24cd12cd7d21eb06a3f8038e47d1d
Eric Denovellis <edeno@bu.edu>
2024-05-23 13:58:07 -0700

    Add other useful classes to init

--- src/non_local_detector/__init__.py
+++ src/non_local_detector/__init__.py
@@ -1,12 +1,29 @@
-from non_local_detector.models import (
-    ContFragClusterlessClassifier,  # noqa
-    ContFragSortedSpikesClassifier,  # noqa
-    MultiEnvironmentSortedSpikesClassifier,  # noqa
-    MultiEnvironmentClusterlessClassifier,  # noqa
-    NonLocalClusterlessDetector,  # noqa
-    NonLocalSortedSpikesDetector,  # noqa
-    ClusterlessDecoder,  # noqa
-    SortedSpikesDecoder,  # noqa
+from non_local_detector.continuous_state_transitions import (  # noqa
+    Discrete,
+    EmpiricalMovement,
+    Identity,
+    RandomWalk,
+    RandomWalkDirection1,
+    RandomWalkDirection2,
+    Uniform,
+)
+from non_local_detector.discrete_state_transitions import (
+    DiscreteNonStationaryCustom,
+    DiscreteNonStationaryDiagonal,
+    DiscreteStationaryCustom,
+    DiscreteStationaryDiagonal,
+)
+from non_local_detector.environment import Environment  # noqa
+from non_local_detector.initial_conditions import UniformInitialConditions  # noqa
+from non_local_detector.models import (  # noqa
+    ClusterlessDecoder,
+    ContFragClusterlessClassifier,
+    ContFragSortedSpikesClassifier,
+    MultiEnvironmentClusterlessClassifier,
+    MultiEnvironmentSortedSpikesClassifier,
+    NonLocalClusterlessDetector,
+    NonLocalSortedSpikesDetector,
+    SortedSpikesDecoder,
 )
 
 try:
