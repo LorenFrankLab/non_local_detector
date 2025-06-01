ed015ab22d4634694333135b2922791662eb3ba3
Eric Denovellis <edeno@bu.edu>
2025-06-01 09:41:16 -0400

    Add useful functions

--- src/non_local_detector/environment/layout/__init__.py
+++ src/non_local_detector/environment/layout/__init__.py
@@ -12,6 +12,7 @@ from .engines.regular_grid import RegularGridLayout
 from .engines.shapely_polygon import ShapelyPolygonLayout
 from .engines.triangular_mesh import TriangularMeshLayout
 from .factories import create_layout, get_layout_parameters, list_available_layouts
+from .helpers.utils import get_centers, get_n_bins
 
 __all__ = [
     "LayoutEngine",
@@ -25,4 +26,6 @@ __all__ = [
     "list_available_layouts",
     "get_layout_parameters",
     "create_layout",
+    "get_centers",
+    "get_n_bins",
 ]
